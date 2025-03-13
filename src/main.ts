25474f67f153d8dd479d91da26d1bf31619da7de
jimao <shihao.gu@perfma.com>
2025-03-13 22:14:24 +0800

    feat: crypto module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CryptoModule } from './agents/crypto/crypto.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create([AppModule, CryptoModule]);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
